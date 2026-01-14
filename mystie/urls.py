99a844ace6eac40bb67a94fedf5781732258d510
BEKA2222 <hiro21874@gmail.com>
2026-01-14 18:30:53 +0600

    update_aws

--- mystie/urls.py
+++ mystie/urls.py
@@ -27,7 +27,6 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Car Auction API",
       default_version='v1',
-      description="Документация для аукциона машин",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="admin@auction.local"),
       license=openapi.License(name="BSD License"),
